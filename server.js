43c76cef5141ad7576c2b6c067c2e2d03269ac1c
gjj52019 <garyjjenks@verizon.net>
2021-09-22 12:29:35 -0400

    used the /:number_of_bottles to solve the problem instead of the for loop

--- server.js
+++ server.js
@@ -6,70 +6,50 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//===================For loop worked but not what the clients wanted ===============
+// //current for loop allows all /# inputs as long as below 98
+// for (let i = 98; i >= 0; i--) {
+//     if (i === 0) {
+//         app.get('/' + i, (req, res) => {
+//             res.send(`
+//             <html>
+//                  <body>
+//                      <p>${i} Bottles of beer on the wall</   p>
+//                  </body>
+//             </html>
+//                      `)
+//         });
+//     }else{
+//     app.get('/' + i, (req, res) => {
+//         res.send(`
+//         <html>
+//             <body>
+//                 <p>${i} Bottles of beer on the wall</p>
+//                 <p><a href="http://localhost:3000/${i = (i - 1)}"> take one down, pass it around</a></p>
+//              </body>
+//         </html>
+//     `)
+//     });
+// }
+// }
+//============= for loop worked but not what the client wanted =================
 
+//much cleaner than the foor loop
+app.get('/:number_of_bottles', (req,res)=>{
 
-
-//============below code under construction======================================
-//tried to pass the user input from the url to the num1 variable and use the parseInt() to pass it as an int for the for loops i to start with but did not work out
-
-/*
-app.get('/:num1',(req,res)=>{
-
-    for (let i = parseInt(req.params.num1); i >= 0; i--) {
-        if (i === 0) {
-            app.get('/' + i, (req, res) => {
-                res.send(`
-                <html>
-                     <body>
-                         <p>${i} Bottles of beer on the wall</   p>
-                     </body>
-                </html>
-                         `)
-            });
-        }else{
-        app.get('/' + i, (req, res) => {
-            res.send(`
-            <html>
-                <body>
-                    <p>${i} Bottles of beer on the wall</p>
-                    <p><a href="http://localhost:3000/${i = (i - 1)}"> take one down, pass it around</a></p>
-                 </body>
-            </html>
-        `)
-        });
-    }
-    }
-
+    let currentBottle = parseInt(req.params.number_of_bottles)
+    //console.log(currentBottle);
+    let nextBottle = currentBottle - 1
+    //console.log(nextBottle)
+    res.send(`
+    <html>
+        <body>
+            <p>${currentBottle} Bottles of beer on the wall</p>
+            <p><a href="http://localhost:3000/${nextBottle}"> take one down, pass it around</a></p>
+         </body>
+    </html>
+`)
 });
-*/
-//================above code under construction========================
-
-
-//current for loop allows all /# inputs as long as below 98
-for (let i = 98; i >= 0; i--) {
-    if (i === 0) {
-        app.get('/' + i, (req, res) => {
-            res.send(`
-            <html>
-                 <body>
-                     <p>${i} Bottles of beer on the wall</   p>
-                 </body>
-            </html>
-                     `)
-        });
-    }else{
-    app.get('/' + i, (req, res) => {
-        res.send(`
-        <html>
-            <body>
-                <p>${i} Bottles of beer on the wall</p>
-                <p><a href="http://localhost:3000/${i = (i - 1)}"> take one down, pass it around</a></p>
-             </body>
-        </html>
-    `)
-    });
-}
-}
 
 //default 
 app.get('/', (req, res) => {
